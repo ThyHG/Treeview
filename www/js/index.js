b1980e23dce167cfe82ed9a60014b99dc533a2a8
ThyHG <jonspruyt@gmail.com>
2015-10-15 16:39:29 +0200

    currency

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -46,4 +28,23 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+};
+
+var events = {
+  addEntry: function() {
+      var textInput = document.getElementById('text-input').value;
+      var d = new Date();
+      var n = d.getDate();
+      console.log(d, n);
+      if(localStorage["entries"].length !== 0) {
+        var entries = JSON.parse(localStorage["entries"]);
+      } else {
+        var entries = [];
+      }
+      entries.push(n+' '+textInput);
+      console.log(entries);
+      localStorage["entries"] = JSON.stringify(entries);
+      var schatkist = JSON.parse(localStorage["entries"]);
+      console.log('schatkist', schatkist);
+  }
 };
